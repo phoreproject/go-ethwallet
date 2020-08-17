cc92bf94befa385001057983aedc7d2f6f4fd2f8
anchaj <anchaj@users.noreply.github.com>
2020-08-17 21:37:31 +0200

    Fix PRIORITY typo

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -645,7 +645,7 @@ func (wallet *EthereumWallet) GetFeePerByte(feeLevel wi.FeeLevel) big.Int {
 		ret, _ = big.NewFloat(est.Average * 100000000).Int(nil)
 	case wi.ECONOMIC, wi.SUPER_ECONOMIC:
 		ret, _ = big.NewFloat(est.SafeLow * 100000000).Int(nil)
-	case wi.PRIOIRTY, wi.FEE_BUMP:
+	case wi.PRIORITY, wi.FEE_BUMP:
 		ret, _ = big.NewFloat(est.Fast * 100000000).Int(nil)
 	}
 	return *ret
